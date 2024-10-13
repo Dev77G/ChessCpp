38251e22200372fa8945771ad97534782658e2f9
Dev77G <99683415+Dev77G@users.noreply.github.com>
2024-10-14 03:22:17 +0530

    Update main.cpp

--- Chess_Code/main.cpp
+++ Chess_Code/main.cpp
@@ -8,7 +8,7 @@ int main()
 	string s;
 	bool newgame = true;
 	cout << "   _____ _    _ ______  _____ _____ \n  / ____| |  | |  ____|/ ____/ ____| \n | |    | |__| | |__  | (___| (___  \n | |    |  __  |  __|  \\___  \\___ \\ \n | |____| |  | | |____ ____) |___) | \n  \\_____|_|  |_|______|_____/_____/ \n" << endl;
-	cout << "A game by George Harter"<<endl;
+	cout << "A game by Dev Gupta"<<endl;
 	cout << "Enter any key to continue" << endl;
 	cin >> s;
 
